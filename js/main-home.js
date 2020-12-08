82fb3b9cb2dc6b67f897376647b64ff7f4347476
Rimantas <belauzas@yahoo.com>
2020-12-08 16:51:32 +0200

    Final initial stucture

--- js/main-home.js
+++ js/main-home.js
@@ -1,1 +1,37 @@
-console.log('Importing modules...');
+/***************
+ALL IMPORTS
+****************/
+/* header */
+/* hero */
+/* features */
+/* app promo */
+/* app promo 2 */
+/* how works */
+/* gallery */
+/* testimonials */
+/* pricing */
+/* faq */
+/* team */
+/* app store */
+/* subscribe */
+/* contact */
+/* footer */
+
+/***************
+EXECUTION
+****************/
+/* header */
+/* hero */
+/* features */
+/* app promo */
+/* app promo 2 */
+/* how works */
+/* gallery */
+/* testimonials */
+/* pricing */
+/* faq */
+/* team */
+/* app store */
+/* subscribe */
+/* contact */
+/* footer */
